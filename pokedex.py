cf9f3da2c6e3361148a586ee52a2e67accf3a928
Ben Mueller <benjm2002@gmail.com>
2022-06-22 22:17:11 -0500

    [Commit time: 6/22/2022, 10:17:11 PM]

--- pokedex.py
+++ pokedex.py
@@ -6,21 +6,36 @@ from selenium.webdriver.common.by import By
 
 import chromedriver_autoinstaller
 
-#chromedriver_autoinstaller.install() # install and add to path
-#driver = webdriver.Chrome()
+# TODO make a version of this that uses the API instead of web scraping
+
 # idea, we could just scrape the data once and save locally in a json file to make lookup super easy
 
 # get the pokemon whose id is in the entry field, clear the field, then display the data
+
+def scrape(poke_id):
+    poke_data = {}
+    chromedriver_autoinstaller.install() # install and add to path
+    driver = webdriver.Chrome()
+    driver.get("https://pokemondb.net/pokedex/all")
+    # use css selectors to find the nth child from the top of the html element that holds all of the rows
+    row = driver.find_element(
+        By.CSS_SELECTOR,
+        f"tr:nth-child({poke_id})"
+    )
+    breakpoint()
+
+    return poke_data
+
+
+
 def get_pokemon():
     # get id from entry field and clear field
-    print(str(entry_var.get()))
-    #id = entry_var
-
+    poke_id = entry_var.get()  # TODO figure out how to get this to work
     # scrape website for data about the id
-    pass
+    poke_data = scrape(poke_id)
+    
 
-
-def init_gui():
+def run_gui():
     global entry_var
     window = tk.Tk()
     window.geometry("400x400")
@@ -34,8 +49,10 @@ def init_gui():
 
 
 def main():
-    init_gui()  # gui is initially empty, prompts user to enter a pokemon ID
-    return 
+    scrape(3)
+    return
+
+    run_gui()  # gui is initially empty, prompts user to enter a pokemon ID
     d = get_pokemon(poke_id)   # poke_id is what user submitted to the gui, this fn returns a dict of data about the pokemon
     # now need to put the data in d in the gui
 
