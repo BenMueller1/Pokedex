718949094ec2c76e41f881637a46f5db59a391ce
Ben Mueller <benjm2002@gmail.com>
2022-06-22 21:39:50 -0500

    [Commit time: 6/22/2022, 9:39:49 PM]

---
+++ pokedex.py
@@ -0,0 +1,19 @@
+import tkinter as tk
+# import selenium 
+
+
+
+# idea, we could just scrape the data once and save locally in a json file to make lookup super easy
+
+
+def main():
+    print('make sure code histories working')
+
+
+
+
+
+
+
+if __name__ == "__main__":
+    main()
