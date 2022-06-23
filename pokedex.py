c30fc6663f3417a76c46d67f02ae208e4feb2406
Ben Mueller <benjm2002@gmail.com>
2022-06-22 21:44:28 -0500

    [Commit time: 6/22/2022, 9:44:27 PM]

--- pokedex.py
+++ pokedex.py
@@ -5,11 +5,15 @@ import selenium
 
 # idea, we could just scrape the data once and save locally in a json file to make lookup super easy
 
+def init_gui():
+    window = tk.Tk()
+
 
 def main():
-    print('make sure code histories working')
-
-
+    init_gui()  # gui is initially empty, prompts user to enter a pokemon ID
+    return 
+    d = get_pokemon(poke_id)   # poke_id is what user submitted to the gui, this fn returns a dict of data about the pokemon
+    # now need to put the data in d in the gui
 
 
 
