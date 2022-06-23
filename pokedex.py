d3c833e522c24068ce1819f5af7f41ce32afb985
Ben Mueller <benjm2002@gmail.com>
2022-06-22 22:01:55 -0500

    [Commit time: 6/22/2022, 10:01:55 PM]

--- pokedex.py
+++ pokedex.py
@@ -14,7 +14,7 @@ entry_var = None
 # get the pokemon whose id is in the entry field, clear the field, then display the data
 def get_pokemon():
     # get id from entry field and clear field
-    print(entry_var)
+    print(entry_var.get())
     #id = entry_var
 
     # scrape website for data about the id
